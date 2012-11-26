76eaa14c2b4ddf793c55ab4f9c25689d71362bfc
elisabetesantos <elisabete.aprcd.santos@gmail.com>
2012-11-26 13:36:05 -0200

    exercise 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21

---
+++ ex7.py
@@ -0,0 +1,24 @@
+print "Mary had a little lamb"
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mry went."
+#multiplies the signal by ten
+print "." * 10 # what'd that do ?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+# if remove the comma every line prints a word. 
+# with the comma the two words are printed together
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
